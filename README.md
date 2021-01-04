23f87a0512e65196ca3703ece2dd9c35338844f0
ryanmaddox <maddoxryan15@gmail.com>
2021-01-04 07:20:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-95k1wi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-95k1wi)
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <ul className="search-result-gridiew-items four-items" data-automation-id="search-result-gridview-items">
+        <li className="grid-col search-gridview-first-col-item search-gridview-first-grid-row-item" data-tl-id="producttileGridView-0">
+          <div data-id="7I0r6FJBIJop" className="search-result-gridview-item-wrapper" role="presentation">
+            <div className="flag-and-heart grid">
+              <div className="search-result-flag fridview flag-container-clearance">
+                <div></div>
+              </div>
+              <div className="feds-heart" data-tl-id="item-tile-heart"></div>
+            </div>
+          </div>
+        
+        </li>
+        
+      </ul>
+      
+    </div>
+  );
+}
